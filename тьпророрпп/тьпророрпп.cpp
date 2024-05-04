c8234203f7f29e5d6c69ef8681cb6f3916113bf4
Администратор <@1->
2024-05-04 22:23:10 +0500

    Добавьте файлы проекта.

---
+++ тьпророрпп/тьпророрпп.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include<vector>
+#include<algorithm>
+#include<sstream>
+
+using namespace std;
+
+
+int checker(string word)
+{
+	int lon = word.length();
+	for (int i = 0; i < lon / 2; i++)
+	{
+		if (word[i] != word[lon - i - 1])
+			return false;
+	}
+	return true;
+}
+
+int main() {
+	while (true) {
+		setlocale(LC_ALL, "ru");
+		string wow;
+		getline(cin, wow); "\n";
+		if (checker(wow))
+		{
+			cout << "Это палидром\n";
+		}
+		else
+		{
+			cout << "Это не палидром\n";
+		}
+	}
+}
